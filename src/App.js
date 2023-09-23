4a9717e21453a59b684ecfb67efdea36c68d39f3
kenny <kennyp41234@gmail.com>
2023-09-23 17:21:31 -0500

    deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/Testimonio.jsx 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   src/styles/App.css 	new file:   src/styles/Testimonio.css 	renamed:    src/index.css -> src/styles/index.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './styles/App.css';
+import Testimonio from './components/Testimonio.jsx';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+
+            </header>
+            <Testimonio />
+            <Testimonio />
+            <Testimonio />
+            <Testimonio />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Testimonio.jsx
@@ -0,0 +1,15 @@
+import '../styles/Testimonio.css';
+
+function Testimonio() {
+    return (
+        <div className="contenedor-testimonio">
+            <img src="https://via.placeholder.com/150" alt="Testimonio" />
+            <div>
+                <h3>Testimonio</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptates. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Testimonio;
